a91e84a14e3433a389e1029c4b42e9cb714a1bfd
enjulia17 <61185004+enjulia17@users.noreply.github.com>
2020-04-04 11:36:45 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include "Complex.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	Complex x;
+	Complex y(3, 2);
+	Complex z(y);
+	cout << x << " " << y << " " << z << " Constructors" << endl;
+	z.set(6, 4);
+	cout << z.get() << " set&&get " << endl;
+	Complex d;
+	d = x + y;
+	cout << d << " operator + " << endl;
+	d = d - y;
+	cout << d << " operator - " << endl;
+	d = z*y;
+	cout << d << " operator * " << endl;
+	d = z / y;
+	cout << " operator / " << d << endl;
+	d = y;
+	cout << d << " operator = " << endl;
+	cout << (d == y) << " operator == " << endl;
+	cout << (x == y) << " operator == " << endl;
+
+	Complex r;
+	cin >> r;
+	cout << r << " operator cin>> " << endl;
+
+	return 0;
+}
